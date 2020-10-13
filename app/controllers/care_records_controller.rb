6a5901e725d2966630af304703aeac0f326963a2
tmtk <blindhurts.6001@gmail.com>
2020-10-13 15:15:13 +0900

    再調整

--- app/controllers/care_records_controller.rb
+++ app/controllers/care_records_controller.rb
@@ -38,7 +38,7 @@ class CareRecordsController < ApplicationController
   end
   private
   def care_record_params
-    params.require(:care_record).permit(:content, :client_id)
+    params.require(:care_record).permit(:content, :client_id, :content_date)
   end
   # idをキーとして値を取得するメソッドを追加
   def set_care_record
