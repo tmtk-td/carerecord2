fab392a725707a10c44847403221a6810465d994
tmtk <blindhurts.6001@gmail.com>
2020-10-15 07:25:59 +0900

    seed追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Label.create!(
-   title: "入浴済み"
-)
-Label.create!(
-   title: "体操参加"
-)
+# Label.create!(
+#    title: "入浴済み"
+# )
+# Label.create!(
+#    title: "体操参加"
+# )
+
+# 5.times do |n|
+#       Client.create!(
+#         name: "男性利用者sample#{n + 1}",
+#         sex: "男性",
+#         image: File.open('./app/assets/images/m_e_others_501.png')
+#       )
+#    end
+
+10.times do |n|
+   Client.create!(
+      name: "女性利用者sample#{n + 1}",
+      sex: "女性",
+      image: File.open('./app/assets/images/m_e_others_501.png')
+   )
+end   
+   
