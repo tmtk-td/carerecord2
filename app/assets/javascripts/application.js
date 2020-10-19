0fc899937cbf1975fab866b7a73656adc04147da
tmtk <blindhurts.6001@gmail.com>
2020-10-19 10:58:39 +0900

    再調整２

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,7 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
+//
 //= require jquery/dist/jquery.js
 //= require rails-ujs
 //= require activestorage
